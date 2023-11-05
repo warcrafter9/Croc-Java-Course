bc03e11fe96a521a7e8de294ab6ca9de899d3d21
Valery Volodin <valery_volo@mail.ru>
2023-11-05 21:41:25 +0500

    Добавлен вывод generateMessage();

--- src/ru/wjs/volodin/practicaltasks4/task7/Main.java
+++ src/ru/wjs/volodin/practicaltasks4/task7/Main.java
@@ -1,8 +1,15 @@
 package ru.wjs.volodin.practicaltasks4.task7;
 
+import ru.wjs.volodin.practicalTasks3.task5.importedproducts.importedwashingmachines.ImportWashingMachine;
+import ru.wjs.volodin.practicalTasks3.task5.products.refrigerators.Fridge;
+
 public class Main {
     public static void main(String[] args) {
-        Order myOrder = new Order("Valery","89194231891","товар1","товар2","товар3");
-        System.out.println(myOrder.generateMessage());
+        Fridge fridge = new Fridge(80_000, "Холодильник...", 65, 32, 70, "Черный",
+                "Россия", true, 0, 8);
+        ImportWashingMachine washingMachineImport=new ImportWashingMachine(50_000,"Стиральная машина",
+                50,30,50, "Белый", "Китай",false,9);
+        Order testOrder = new Order("Valery", "89193412871", fridge,washingMachineImport);
+        System.out.println( testOrder.generateMessage());
     }
 }
