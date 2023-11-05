560cea332d3573087d860af8c67b605570dd5855
Valery Volodin <valery_volo@mail.ru>
2023-11-05 21:50:07 +0500

    Добавлены комментарии, исключение

--- src/ru/wjs/volodin/practicaltasks4/task7/OrderChecker.java
+++ src/ru/wjs/volodin/practicaltasks4/task7/OrderChecker.java
@@ -1,6 +1,20 @@
 package ru.wjs.volodin.practicaltasks4.task7;
 
 public interface OrderChecker {
-    void checkOrderExpiration(); // проверка и установка статуса заказов
+    /**
+     * Проверяет возможность получения заказа в зависимости от его статуса.
+     *
+     * @return Возможность получения заказа.
+     * <p> При COLLECTED с истекшим сроком - бросает исключение OrderExpirationException
+     */
+    boolean isReadyForPickup() throws OrderExpirationException;
+
+    /**
+     * Проверка заказа на истечение срока хранения.
+     * <p>
+     * При истечении - устанавливает соответсвующий статус.
+     * @return true - товар истёк, false - ещё нет.
+     */
+    boolean checkAndSetOrderExpiration();
 
 }
