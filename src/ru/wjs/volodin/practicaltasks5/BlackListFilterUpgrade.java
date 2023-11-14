42fa64c0405ee442a3aa96ca40d66a9a2ab86d6d
Valery Volodin <valery_volo@mail.ru>
2023-11-14 15:13:19 +0500

    Нечувствительность к регистру + замена слова полностью.

--- src/ru/wjs/volodin/practicaltasks5/BlackListFilterUpgrade.java
+++ src/ru/wjs/volodin/practicaltasks5/BlackListFilterUpgrade.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import static ru.wjs.volodin.practicaltasks5.BlackListFilterMasker.createMask;
+
 public class BlackListFilterUpgrade implements BlackListFilter {
 
 
@@ -11,36 +13,32 @@ public class BlackListFilterUpgrade implements BlackListFilter {
     public void filterComments(List<String> comments, Set<String> blackList) {
         List<String> filteredComments = new ArrayList<>();
         for (String comment : comments) {
-            StringBuilder maskedComment = new StringBuilder(comment);
             for (String word : comment.split("[;,\n  ]+")) {
                 for (String banWord : blackList) {
-                    if (checkErrorInBadWord(banWord, word.toLowerCase())) {
-                        int firstIndex = maskedComment.indexOf(word);
-                        int lastIndex = firstIndex + word.length();
-                        for (int i = firstIndex; i < lastIndex; i++) {
-                            maskedComment.setCharAt(i, '*');
-                        }
+                    if (checkErrorInBanWord(banWord, word)) {
+                        comment = comment.replaceAll(word, createMask(word));
                     }
                 }
             }
-            filteredComments.add(String.valueOf(maskedComment));
+            filteredComments.add(comment);
         }
         comments.clear();
         comments.addAll(filteredComments);
     }
 
-    public static boolean checkErrorInBadWord(String word1, String word2) {
+    public static boolean checkErrorInBanWord(String banWord, String word) {
         int countErrors = 0;
-        if (word1.length() != word2.length()) {
+        if (banWord.length() != word.length()) {
             return false;
         }
-        for (int i = 0; i < word2.length(); i++) {
-            if (!(word1.charAt(i) == word2.charAt(i))) {
-                countErrors++;
+        for (int i = 0; i < word.length(); i++) {
+            if (!((banWord.charAt(i) == word.charAt(i) || banWord.charAt(i) == word.charAt(i) + 32) || banWord.charAt(i) == word.charAt(i) - 32)) {
+                countErrors++; // тут сравнивается символ с верхним и нижним регистром
             }
         }
         return countErrors <= 1;
     }
+
 
 }
 
