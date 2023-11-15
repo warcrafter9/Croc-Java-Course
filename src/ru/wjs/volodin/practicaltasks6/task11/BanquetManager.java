5d6d47cb673182c0b9ec4655d36c6bbad9ad916f
Valery Volodin <valery_volo@mail.ru>
2023-11-15 19:28:27 +0500

    Добавлена возможность создавать специальные требования к меню через Predicate

--- src/ru/wjs/volodin/practicaltasks6/task11/BanquetManager.java
+++ src/ru/wjs/volodin/practicaltasks6/task11/BanquetManager.java
@@ -1,47 +1,77 @@
 package ru.wjs.volodin.practicaltasks6.task11;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
 
 
 public class BanquetManager {
-    static HashMap<Cook, List<Dish>> cookListHashMap;
-   public BanquetManager(){
-       cookListHashMap = new HashMap<>();
+   public static HashMap<Cook, List<Dish>> cookListHashMap;
+
+    public BanquetManager() {
+        cookListHashMap = new HashMap<>();
     }
-   public void addCook(Cook cook, List<Dish> dishes){
-       cookListHashMap.put(cook,dishes);
+
+    public void addCook(Cook cook, List<Dish> dishes) {
+        cookListHashMap.put(cook, dishes);
     }
-    public void removeCook(Cook cook){
-       cookListHashMap.remove(cook);
+
+    public void removeCook(Cook cook) {
+        cookListHashMap.remove(cook);
     }
-    public static List<Dish> createMenu(List<Cook> workingCooks, List<String> missingIngredients, int numberDishes){
+
+    public static List<Dish> createMenu(List<Cook> workingCooks, List<String> missingIngredients, int numberDishes) {
         List<Dish> menu = new ArrayList<>();
-        for(Cook cook:workingCooks){
+        for (Cook cook : workingCooks) {
             List<Dish> dishes = cookListHashMap.get(cook);
-            for(Dish dish:dishes){
-                if(canCook(dish,missingIngredients)){
+            for (Dish dish : dishes) {
+                if (canCook(dish, missingIngredients)) {
                     menu.add(dish);
                 }
             }
         }
         menu.sort(Comparator.reverseOrder());
 
-        return menu.subList(0,Math.min(menu.size(), numberDishes)); // чтоб не оставлять полупустой список, если вдруг блюд не хватит
+        return menu.subList(0, menu.size());
     }
+
+    /**
+     * @param requirement        особые требования к блюду.
+     * @param workingCooks       рабочие повара сегодня
+     * @param missingIngredients недостающие ингредиенты
+     * @param numberDishes       необходимое количество блюд
+     * @return меню с учетом капризов его высочества
+     */
+    public static List<Dish> createMenuWithSpecialRequirements(Predicate<Dish> requirement, List<Cook> workingCooks, List<String> missingIngredients, int numberDishes) {
+        List<Dish> menu = createMenu(workingCooks, missingIngredients, numberDishes);
+        List<Dish> menuWithRequirements = new ArrayList<>();
+        for (Dish dish : menu) {
+            if (requirement.test(dish)) {
+                menuWithRequirements.add(dish);
+            }
+        }
+        return menuWithRequirements;
+    }
+
     private static boolean canCook(Dish dish, List<String> missingIngredients) {
-            for (String ingredient : dish.getIngredients()) {
-                for (String missingIngredient : missingIngredients) {
-                    if (ingredient.equalsIgnoreCase(missingIngredient)) {
-                        return false;
-                    }
+        for (String ingredient : dish.getIngredients()) {
+            for (String missingIngredient : missingIngredients) {
+                if (ingredient.equalsIgnoreCase(missingIngredient)) {
+                    return false;
                 }
             }
-            return true;
         }
+        return true;
     }
+
+    public static Predicate<Dish> createRequirementStrawberry() {
+        return dish -> {
+            if (dish.getIngredients().contains("Клубника")) {
+                return true;
+            }
+            return false; // то есть, если оно совпадет, то моим дефолтным методом не будет добавлено в отфильтрованные коммент.
+        };
+    }
+}
 
 
--- src/ru/wjs/volodin/practicaltasks6/task11/Main.java
+++ src/ru/wjs/volodin/practicaltasks6/task11/Main.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static ru.wjs.volodin.practicaltasks6.task11.DishCategory.DESSERT;
-import static ru.wjs.volodin.practicaltasks6.task11.DishCategory.MAINDISH;
+import static ru.wjs.volodin.practicaltasks6.task11.BanquetManager.createMenuWithSpecialRequirements;
+import static ru.wjs.volodin.practicaltasks6.task11.BanquetManager.createRequirementStrawberry;
+import static ru.wjs.volodin.practicaltasks6.task11.DishCategory.*;
 
 public class Main {
     public static void main(String[] args) {
         Cook cook1 = new Cook("Алексей");
         Cook cook2 = new Cook("Александра");
+        Cook cook3 = new Cook("Анатолий");
 
-        Dish dish1 = new Dish("Торт", Arrays.asList("Орехи", "Коржи"), DESSERT, 92, 95);
-        Dish dish2 = new Dish("Стейк", Arrays.asList("Мясо", "Соль", "Перец"), MAINDISH, 95, 88);
-        Dish dish3 = new Dish("что-то шоколадное", Arrays.asList("Шоколад", "Коржи", "Сахар"), DESSERT, 92, 90);
+        Dish dish1 = new Dish("Торт", Arrays.asList("Орехи", "Коржи","Клубника"), DESSERT, 75, 95);
+        Dish dish2 = new Dish("Стейк", Arrays.asList("Мясо", "Соль", "Перец"), MAINDISH, 99, 88);
+        Dish dish3 = new Dish("Тарталетки", Arrays.asList("Яйца", "Майонез","Колбаса"), SNACK, 75, 95);
+        Dish dish4 = new Dish("что-то шоколадное", Arrays.asList("Шоколад", "Мука", "Сахар","Клубника"), DESSERT, 92, 90);
+        Dish dish5 = new Dish("Клубничный мусс", Arrays.asList("Сливки", "Вода", "Сахар","Клубника"), DESSERT, 92, 92);
 
       //  ArrayList dishesCook1= new ArrayList<>();  -> Arrays.asList("")
        // dishesCook1.add(dish1);
@@ -22,14 +26,25 @@ public class Main {
         BanquetManager banquetManager = new BanquetManager();
         banquetManager.addCook(cook1, Arrays.asList(dish1, dish2));
         banquetManager.addCook(cook2, Arrays.asList(dish2, dish3));
+        banquetManager.addCook(cook3,Arrays.asList(dish4,dish5));
 
-        List<Cook> workingCooks = Arrays.asList(cook1, cook2);
-        List<String> missingIngredients = Arrays.asList("Соль","Перец");
-        int numberOfDishes = 2;
+        List<Cook> workingCooks1 = Arrays.asList(cook1, cook2);
+        List<Cook> workingCooks2 = Arrays.asList(cook1, cook3);
 
-        List<Dish> menu = BanquetManager.createMenu(workingCooks,missingIngredients,numberOfDishes);
-        System.out.println("Меню: ");
+        List<String> missingIngredients1 = Arrays.asList("Соль","Перец");// -стейк
+        List<String> missingIngredients2 = Arrays.asList("Вода"); // -клубничный мусс
+        int numberOfDishes1=4;
+        int numberOfDishes2=3;
+
+
+        List<Dish> menu = BanquetManager.createMenu(workingCooks1,missingIngredients1,numberOfDishes1);
+        List<Dish> menuWithSpecialRequirements = createMenuWithSpecialRequirements(createRequirementStrawberry(),workingCooks2,missingIngredients2,numberOfDishes2);
+        System.out.println("Меню - 1 (без специальных требований): ");
         for (Dish dish : menu) {
+            System.out.println("- " + dish.getName());
+        }
+        System.out.println("Меню - 2 (со специальными требованиями): ");
+        for (Dish dish : menuWithSpecialRequirements) {
             System.out.println("- " + dish.getName());
         }
 
